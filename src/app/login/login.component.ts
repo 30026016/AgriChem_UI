b247d96a570840dbf2aaf778c4ee8ec3d883ba16
Jay Vipulkumar Patel <30029533@student.toiohomai.ac.nz>
2020-11-06 13:33:43 +1300

    Updated Code Jay

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { LoginService } from './login.service';
 
 @Component({
   selector: 'app-login',
@@ -10,19 +12,33 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 export class LoginComponent implements OnInit {
 
   loginForm: FormGroup;
-  
-  constructor(private fb: FormBuilder, private http: HttpClient) { }
+  userList;
+
+  constructor(private fb: FormBuilder, private http: HttpClient, private loginService: LoginService, private router: Router) { }
 
   ngOnInit(): void {
     this.loginForm = this.fb.group({
-      userName : [''],
-      password : ['']
+      userName: [''],
+      password: ['']
     });
   }
 
   // tslint:disable-next-line: typedef
-  OnSubmit(){
-    console.log(this.loginForm.value.userName + ' ' + this.loginForm.value.password);
+  OnSubmit() {
+
+    if (this.loginForm.value.userName == this.userList[0].userName && this.loginForm.value.password == this.userList[0].passWord) {
+      this.router.navigateByUrl('/agri-application');
+    }
+    else {
+      console.log(this.userList);
+      alert("Your Username or Password is Invalid !");
+    }
   }
 
+  GetUser() {
+    this.loginService.getAllUser().subscribe(data => {
+      this.userList = data;
+    });
+
+  }
 }
